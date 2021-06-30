4b8fb1be01da96342dea4c661b4ffdcf57fa12bc
alestor123 <alestoraldous@gmail.com>
2021-06-30 22:27:02 +0530

    added main code

---
+++ App.js
@@ -0,0 +1,18 @@
+'use strict'
+const Xray = require('x-ray')
+const xray = Xray()
+
+module.exports = async (username, pgrange) => {
+  if (!(username && typeof username === 'string' && username.length > 0)) throw new Error('Please enter a vaild username')
+  else if (!(pgrange && typeof pgrange === 'number' && pgrange > 0)) throw new Error('Enter a valid page number range')
+  const data = await getSponsors(username, Math.floor(Math.random() * pgrange) + 1)
+  return data[Math.floor(Math.random() * data.length)]
+}
+async function getSponsors (username, page) {
+  const data = await xray(`https://github.com/sponsors/${username}/sponsors_partial?page=${page}`, 'img', [{
+    title: '@src',
+    username: '@alt'
+  }])
+  data.forEach(user => { user.username = user.username.substring(1) })
+  return data
+}
