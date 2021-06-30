6de58f97b5cf7cba9a79cb950ae70f15d4e544a9
alestor123 <alestoraldous@gmail.com>
2021-06-30 22:47:17 +0530

    added cli

--- cli.js
+++ cli.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk')
+const ghn = require('./App');
+
+(async () => {
+  const { username } = await ghn(process.argv[2], process.argv[3] || 1)
+  console.log(chalk.greenBright.bold(`Thanks you ${username} for sponsoring ${process.argv[2]}`))
+})()
